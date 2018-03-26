a34c9611dd20a2f7a44d3e93fa36a38872db7eae
Dustin Van Tate Testa <toast27@gmail.com>
2018-03-26 00:58:27 -0400

    add fire2012 mode

--- modes.h
+++ modes.h
@@ -24,7 +24,8 @@ namespace mode0 {
     else if (v2 <= 0 && curHi == 2)
       curHi = 0;
   }
-  
+
+  // relevant variables
   struct {
     unsigned int offset : 24;
     uint8_t curHi;
@@ -165,14 +166,62 @@ namespace mode2 {
 }
 
 
+// based off of fire2012 demo
+// https://github.com/FastLED/FastLED/blob/master/examples/Fire2012/Fire2012.ino
 namespace mode3 {
 
+  // temperature readings
+  uint8_t l_heat[NUM_LEDS];
+  uint8_t r_heat[NUM_LEDS];
+
+  void fire(CRGB leds[NUM_LEDS], uint8_t heat[NUM_LEDS], bool reverse = false) {
+    
+    // COOLING: How much does the air cool as it rises?
+    // Less cooling = taller flames.  More cooling = shorter flames.
+    // Default 50, suggested range 20-100 
+    const uint8_t cooling = 55;
+    
+    // SPARKING: What chance (out of 255) is there that a new spark will be lit?
+    // Higher chance = more roaring fire.  Lower chance = more flickery fire.
+    // Default 120, suggested range 50-200.
+    const uint8_t sparking = 120;
+
+    // Step 1.  Cool down every cell a little
+    for (int i = 0; i < NUM_LEDS; i++)
+      heat[i] = qsub8( heat[i],  random8(0, ((cooling * 10) / NUM_LEDS) + 2));
   
-  void init(){
+    // Step 2.  Heat from each cell drifts 'up' and diffuses a little
+    for (int k = NUM_LEDS - 1; k >= 2; k--)
+      heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2] ) / 3;
     
+    // Step 3.  Randomly ignite new 'sparks' of heat near the bottom
+    if(random8() < sparking) {
+      int y = random8(7);
+      heat[y] = qadd8(heat[y], random8(160,255) );
+    }
+
+    // Step 4.  Map from heat cells to LED colors
+    for( int j = 0; j < NUM_LEDS; j++) {
+      
+      CRGB color = HeatColor(heat[j]);
+      
+      int pixelnumber;
+      
+      if (reverse) {
+        pixelnumber = (NUM_LEDS-1) - j;
+      } else {
+        pixelnumber = j;
+      }
+      
+      leds[pixelnumber] = color;
+    }
   }
+  
+  void init(){ }
+
   void periodic(){
-    
+    fire(l_leds, l_heat);
+    fire(r_leds, r_heat);
   }
 }
 
